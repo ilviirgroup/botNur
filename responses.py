6c6f35200cc840da0e448ff4c7260a4347d51bb4
ilviirgroup <ilviirgroup@gmail.com>
2021-05-27 03:26:16 +0600

    g

--- responses.py
+++ responses.py
@@ -14,7 +14,7 @@ def sample_responses(input_text):
     user_message = str(input_text).lower()
 
     if user_message in ('hello','hi','sup',):
-        return "Hey! How`s it going"
+        return "Hell! How are you"
     if user_message in ('time','time?'):
         now = datetime.now()
         date_time = now.strftime("%d/%m/%y, %H:%M:%S")
